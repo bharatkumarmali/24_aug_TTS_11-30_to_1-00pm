318f6356f9f4ebeb37af475491b2128786b4dc61
Bharat Kumar <sbharat2143@gmail.com>
2024-05-29 09:51:59 +0530

    Update AMangeservice.jsx

--- src/admin/Pages/AMangeservice.jsx
+++ src/admin/Pages/AMangeservice.jsx
@@ -18,37 +18,36 @@ function AMangeservice() {
     }
 
     // delete 
-    const deletehandle=async(id)=>{
+    const deletehandle = async (id) => {
         const res = await axios.delete(`http://localhost:3000/service/${id}`)
         console.log(res.data)
     }
 
     // edit 
-    const [editingservice,seteditngservice]=useState(null)
-    const [editedservice,seteditedservice]= useState({
-        id:"",
-        service_name:"",
-        desc:"",
-        ser_img:""
+    const [editingservice, seteditngservice] = useState(null)
+    const [editedservice, seteditedservice] = useState({
+        id: "",
+        service_name: "",
+        desc: "",
+        ser_img: ""
     })
 
-    function saveedit(service)
-    {
+    function saveedit(service) {
         seteditngservice(service)
         seteditedservice(service)
-        
+
     }
 
-    const savesumithadle=async(e)=>{
+    const savesumithadle = async (e) => {
         try {
             e.preventDefault()
-            await axios.put(`http://localhost:3000/service/${editingservice.id}`,editedservice)
-           
+            await axios.put(`http://localhost:3000/service/${editingservice.id}`, editedservice)
+
             fetch() // refresh
             seteditngservice(null)
             console.log("update succeefully")
         } catch (error) {
-            console.error("error api not res",error)
+            console.error("error api not res", error)
         }
     }
 
@@ -77,8 +76,8 @@ function AMangeservice() {
                                             <td>{value.desc}</td>
                                             <td>
                                                 <button className='btn btn-primary'>View</button>
-                                                <button className='btn btn-success mx-2' onClick={()=>saveedit(value)}>Edit</button>
-                                                <button className='btn btn-danger' onClick={()=>deletehandle(value.id)}>Delete</button>
+                                                <button className='btn btn-success mx-2' onClick={() => saveedit(value)}>Edit</button>
+                                                <button className='btn btn-danger' onClick={() => deletehandle(value.id)}>Delete</button>
                                             </td>
                                         </tr>
                                     )
@@ -90,40 +89,40 @@ function AMangeservice() {
                     </table>
                     {
                         editingservice && (
-                           <div className="edit-from">
-                             <div className="col-lg-12 quote-text py-5 wow fadeIn" data-wow-delay="0.5s">
-                                <div className="p-lg-5 pe-lg-0">
-                                    <div className="section-title text-start">
-                                        <h1 className="display-5 mb-4">update Sevice</h1>
-                                    </div>
-                                    
-                                    <form >
-                                        <div className="row g-3">
-                                            <div className="col-12 col-sm-6">
-                                                <input type="text"  value={editedservice.service_name} onChange={(e)=>seteditedservice({...editedservice,service_name:e.target.value})} className="form-control border-0" placeholder="add service name" style={{ height: 55 }} />
-                                            </div>
-                                            <div className="col-12 col-sm-6">
-                                                <input  type="url" value={editedservice.ser_img} onChange={(e)=>seteditedservice({...editedservice,ser_img:e.target.value})} className="form-control border-0" placeholder="add imgae" style={{ height: 55 }} />
-                                            </div>
-                                        
-                                            <div className="col-12">
-                                                <textarea  value={editedservice.desc} onChange={(e)=>seteditedservice({...editedservice,desc:e.target.value})}   className="form-control border-0" placeholder="add desc" defaultValue={""} />
-                                            </div>
-                                            <div className="col-12">
-                                                <div className="row">
-                                                    <div className="col-6">
-                                                    <button className="btn btn-primary w-100 py-3" type="submit" onClick={savesumithadle}>update</button>
-                                                    </div>
-                                                    <div className="col-6">
-                                                    <button className="btn btn-primary w-100 py-3" onClick={()=>seteditngservice(null)}>cancel</button>
+                            <div className="edit-from">
+                                <div className="col-lg-12 quote-text py-5 wow fadeIn" data-wow-delay="0.5s">
+                                    <div className="p-lg-5 pe-lg-0">
+                                        <div className="section-title text-start">
+                                            <h1 className="display-5 mb-4">update Sevice</h1>
+                                        </div>
+
+                                        <form >
+                                            <div className="row g-3">
+                                                <div className="col-12 col-sm-6">
+                                                    <input type="text" value={editedservice.service_name} onChange={(e) => seteditedservice({ ...editedservice, service_name: e.target.value })} className="form-control border-0" placeholder="add service name" style={{ height: 55 }} />
+                                                </div>
+                                                <div className="col-12 col-sm-6">
+                                                    <input type="url" value={editedservice.ser_img} onChange={(e) => seteditedservice({ ...editedservice, ser_img: e.target.value })} className="form-control border-0" placeholder="add imgae" style={{ height: 55 }} />
+                                                </div>
+
+                                                <div className="col-12">
+                                                    <textarea value={editedservice.desc} onChange={(e) => seteditedservice({ ...editedservice, desc: e.target.value })} className="form-control border-0" placeholder="add desc" defaultValue={""} />
+                                                </div>
+                                                <div className="col-12">
+                                                    <div className="row">
+                                                        <div className="col-6">
+                                                            <button className="btn btn-primary w-100 py-3" type="submit" onClick={savesumithadle}>update</button>
+                                                        </div>
+                                                        <div className="col-6">
+                                                            <button className="btn btn-primary w-100 py-3" onClick={() => seteditngservice(null)}>cancel</button>
+                                                        </div>
                                                     </div>
                                                 </div>
                                             </div>
-                                        </div>
-                                    </form>
+                                        </form>
+                                    </div>
                                 </div>
                             </div>
-                           </div>
                         )
                     }
                 </div>
